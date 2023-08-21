37c446498ba4ed7a45c003a4a8609962bc08a2fe
Christian Kugler <syphdias+git@gmail.com>
2023-08-22 00:42:06 +0200

    Fix more typos form previous name

--- prenv-p10k-segment.zsh
+++ prenv-p10k-segment.zsh
@@ -1,4 +1,4 @@
-prompt_penv() {
-    [[ -n "${#PENV[@]}" ]] \
-        && p10k segment -f 197 -t "$PENV"
+prompt_prenv() {
+    [[ -n "${#PRENV[@]}" ]] \
+        && p10k segment -f 197 -t "$PRENV"
 }
